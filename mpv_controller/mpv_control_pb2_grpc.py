abd609fd0d9710a07f6e87422c7b3ded26b2ea30
James Brayton <james.brayton@gmail.com>
2026-01-30 11:49:38 -0700

    Fixing issue with protobuf imports.

--- mpv_controller/mpv_control_pb2_grpc.py
+++ mpv_controller/mpv_control_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import mpv_control_pb2 as mpv__control__pb2
+from . import mpv_control_pb2 as mpv__control__pb2
 
 GRPC_GENERATED_VERSION = '1.76.0'
 GRPC_VERSION = grpc.__version__
